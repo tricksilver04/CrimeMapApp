747458fec890d1edfe87cba0405af9d7b8923cc8
john <john.elevado@salarium.com>
2020-11-06 03:05:09 +0800

    added dialog for crime details

--- app/src/main/java/com/example/challenge/ui/adapters/CrimeListAdapter.kt
+++ app/src/main/java/com/example/challenge/ui/adapters/CrimeListAdapter.kt
@@ -3,8 +3,10 @@ package com.example.challenge.ui.adapters;
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.challenge.api.crimes.dto.CrimeModel
+import com.example.challenge.databinding.DialogCrimeDetailsBinding
 import com.example.challenge.databinding.LayoutCrimeRowBinding
 import java.util.ArrayList
 
@@ -32,6 +34,25 @@ class CrimeListAdapter (private val context: Context, private val items: ArrayLi
         fun bind(item: CrimeModel.CrimeData){
             layoutUserBinding.tvName.text = "Crime:\t[${item.id}] ${item.category}"
             layoutUserBinding.tvAddress.text = "Location:\t${item.location.street.name}"
+            layoutUserBinding.root.setOnClickListener {
+                val binding = DialogCrimeDetailsBinding
+                    .inflate(LayoutInflater.from(layoutUserBinding.root.context))
+                binding.tvCategory.text = "Category: ${item.category}"
+                binding.tvId.text = "Category: ${item.id}"
+                binding.tvLocation.text = "Location: ${item.location.latitude},${item.location.longitude}"
+                binding.tvStreetName.text = "Street: ${item.location.street.name}"
+                binding.tvStreetId.text = "Street Id: ${item.location.street.id}"
+                binding.tvLocationType.text = "Location Type: ${item.location_type}"
+                binding.tvMonth.text = "Month: ${item.month}"
+
+                val builder = AlertDialog.Builder(layoutUserBinding.root.context)
+                builder.setTitle("Details")
+                builder.setView(binding.root)
+                builder.setPositiveButton("OK") { dialog, _ ->
+                   dialog.dismiss()
+                }
+                builder.show()
+            }
         }
     }
 }
